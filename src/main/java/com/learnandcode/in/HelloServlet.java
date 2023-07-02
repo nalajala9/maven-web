2219f86cb3f49850a7bcd61a37066b566c8c20e1
ravi <nalajalaravi99@gmail.com>
2023-07-02 17:13:19 +0530

    fixed vunerabilities in the code

--- src/main/java/com/learnandcode/in/HelloServlet.java
+++ src/main/java/com/learnandcode/in/HelloServlet.java
@@ -9,18 +9,22 @@ import java.io.*;
 public class HelloServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
 
         // Read the HTML file
-        InputStream inputStream = getServletContext().getResourceAsStream("/index.html");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        try (InputStream inputStream = getServletContext().getResourceAsStream("/index.html");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+             PrintWriter writer = response.getWriter()) {
 
-        PrintWriter writer = response.getWriter();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            writer.println(line);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.println(line);
+            }
+        } catch (IOException e) {
+            // Handle the exception appropriately, e.g., logging or displaying an error message
+            e.printStackTrace();
         }
-        writer.close();
     }
 }
